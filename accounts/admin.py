4ac2899ba5183182f9d2d2eae930bb2b1fa95ae9
Sepehr Madadi <161921379+threepi6@users.noreply.github.com>
2026-05-02 13:02:28 +0330

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import UserModel, UserProfile
 
 admin.site.register(UserModel)
+admin.site.register(UserProfile)
 
 # Register your models here.
